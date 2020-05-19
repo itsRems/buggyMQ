605bf527b84f04419dc685673b5b0761bc23af02
itsRems <contact@itsrems.com>
2020-05-19 19:42:59 +0200

    added fixed endpoint

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-The api service runs on port 3500 and has two endpoints: / (gets a random number - one event) and /crash (does 3 event requests, only 2 are called, the 2nd never answers)
+The api service runs on port 3500 and has three endpoints:
+/ - gets a random number - one event
+/crash - does 3 event requests, only 2 are called, the 2nd never answers
+/fix - Same as /crash but with some time between the calls
 
 The code has some comments to show where/when it crashes.
--- api/src/router.ts
+++ api/src/router.ts
@@ -5,7 +5,7 @@ const router = Router();
 
 router.get('/', async (req, res) => {
   const { number } = await getEventManager().emitAndWait('GET_RANDOM_NUMBER', {});
-  res.send(`The number is: ${number}. <br> This worked ! Now trying hitting /crash`);
+  res.send(`The number is: ${number}. <br> This worked ! Now trying hitting /crash or /fixed`);
 })
 
 router.get('/crash', async (req, res) => {
@@ -15,6 +15,15 @@ router.get('/crash', async (req, res) => {
   console.log(number); // Does not log
   const { answer } = await getEventManager().emitAndWait('GET_MULTIPLIED_NUMBER', { number }); // Not even called !
   res.send(`The number is: ${answer}`);
+})
+
+router.get('/fix', async (req, res) => {
+  const notUsed = await getEventManager().emitAndWait('GET_NAME', {});
+  await new Promise(resolve => (setTimeout(resolve, 250)))
+  const { number } = await getEventManager().emitAndWait('GET_RANDOM_NUMBER', {});
+  await new Promise(resolve => (setTimeout(resolve, 250)))
+  const { answer } = await getEventManager().emitAndWait('GET_MULTIPLIED_NUMBER', { number }); // Not even called !
+  res.send(`The number is: ${answer}`); // This endpoint works but as we can see I had to add 250ms of waiting time (might be able to be less but this is a low as I went for now)
 })
 
 export default router;
